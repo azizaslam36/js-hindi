3520293effe20665f2c060578ac73f5b4f8bc1c8
MOHD AZIZ ASLAM <mohdazizaslam9@gmail.com>
2025-08-08 11:22:56 +0000

    all new codes

---
+++ 01-basic/04_comparision.js
@@ -0,0 +1,12 @@
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// === strick cheack
+
+console.log('5' == 5); // true
+console.log('5' === 5); // false
